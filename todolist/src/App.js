0928d8f1bb717057743c9ca064e07f9f4dba6177
SamuelOgundele <129412263+SamuelOgundele@users.noreply.github.com>
2024-12-13 00:47:09 +0000

    Updated App.js to add pages folder

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import TaskList from './components/TaskList';
-import TaskForm from './components/TaskForm';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import AddTask from './pages/AddTask';
+import EditTask from './pages/EditTask';
 
-function App() {
+const App = () => {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<TaskList />} />
-                <Route path="/new" element={<TaskForm />} />
+                <Route path="/" element={<Home />} />
+                <Route path="/add" element={<AddTask />} />
+                <Route path="/edit/:id" element={<EditTask />} />
             </Routes>
         </Router>
     );
-}
+};
 
 export default App;
+
 
